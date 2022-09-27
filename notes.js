f6a60771ffe8f0720485cd7a0584c3c18d943a6e
unknown <erskine.duenas@gmail.com>
2022-09-27 10:33:21 +0800

    update

--- notes.js
+++ notes.js
@@ -1,1 +1,21 @@
 console.log("Hello, World");
+
+/* 
+How to create a react project:
+
+git bash in the folder you want to create a react app
+
+in command line: 
+
+step 1: 
+$npx create-react-app *folder name*  //then the *folder name* will be created
+
+step 2:
+$npm start    //new tab will open in your browser "http://localhost:3000/" with the react logo
+
+
+
+
+
+
+*/
