6c06229a3c5bced9188e33b4665aa6666ff64d10
unknown <erskine.duenas@gmail.com>
2022-09-27 12:37:16 +0800

    update

--- Projects/react-course/src/components/ToDo.js
+++ Projects/react-course/src/components/ToDo.js
@@ -1,7 +1,7 @@
 function ToDo(props) {    //props is used as input to have a dynamic approach
     return (
         <div className='card'>
-        <h2>{props.text}</h2>    {/*props.text is used because props is in the input of the function, then it will make every "text" in App.js to be dynamic */}
+        <h2>{props.text}</h2>    {/*props.text is used because props is in the input of the function, then it will make every "text" in App.js to be dynamic **/}
         <div className='actions'>
             <button className='btn'>Delete</button>
         </div>
