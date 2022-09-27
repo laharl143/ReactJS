b15284463d17de2832b595ae8cf8fc5d30a14fef
unknown <erskine.duenas@gmail.com>
2022-09-27 12:46:34 +0800

    update

--- Projects/react-course/src/components/ToDo.js
+++ Projects/react-course/src/components/ToDo.js
@@ -1,9 +1,11 @@
 function ToDo(props) {    //props is used as input to have a dynamic approach
+    function deleteHandler() {}
+
     return (
         <div className='card'>
-        <h2>{props.text}</h2>    {/*props.text is used because props is in the input of the function, then it will make every "text" in App.js to be dynamic **/}
+        <h2>{props.text}</h2>    {/*props.text is used because props is in the input of the function, then it will make every "text" in App.js to be dynamic */}
         <div className='actions'>
-            <button className='btn'>Delete</button>
+            <button className='btn' onClick={deleteHandler}>Delete</button> {/*{} curly braces on react means you can execute a javascript file in it */}
         </div>
     </div>
     );
